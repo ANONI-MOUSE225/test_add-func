0a60c2b2c10201e8981cd5dcdec07d852466346a
yogi <4215078yogi@gmail.com>
2021-04-28 12:03:48 +0900

    second

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,60 @@
+//ハンバーガーメニュー
+function hambuger(){}
+
+document.getElementById("humburger-contents").classList.toggle("")
+
+
+//モーダル 
 let btn = document.getElementById("btn");
 let modal = document.getElementById("modal");
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     modal.style.display = "block";
 })
 
-let backbtn = document.getElementById("back-btn");
+let submitBtn = document.getElementById("submit-btn");
+let modalComp = document.getElementById("modal-complete");
 
-backbtn.addEventListener("click", function(){
-    modal.style.display ="none";
+submitBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+    modalComp.style.display = "block";
 })
 
+let backBtn = document.getElementById("back-btn");
+let backBtn2 = document.getElementById("back-btn2");
+
+backBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+})
+
+backBtn2.addEventListener("click", function () {
+    modalComp.style.display = "none";
+})
+
+let closeBtn = document.getElementById("close-btn");
+
+closeBtn.addEventListener("click", function () {
+    modalComp.style.display = "none";
+})
+
+//アコーディオン
+document.addEventListener("DOMContentLoaded",() => {
+    const question = document.querySelectorAll('.js-question');
+    const answer = document.querySelectorAll('.answer');
+
+    for (let i = 0; i < question.length; i++){
+        let questionEach = question[i];
+        let answerEach = answer[i];
+        questionEach.addEventListener('click', () => {
+            questionEach.classList.toggle('is-active');
+            answerEach.classList.toggle('is-open');
+        });
+    }
+});
+
+
+
+
+  
+  
+  
